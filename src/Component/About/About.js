0e5e42dd3b3b5da0a4c73491bb407cbc75f1075d
Muhammad Haseeb <Mohammad-Haseeb>
2020-12-07 12:18:05 +0500

    font add

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -9,13 +9,13 @@ export default function About(){
           <NavHead/>
           <div className="backImage">
                 <div>             
-                   <h1 style={{paddingTop:"10%",textAlign:"center"}}>WIPERTS</h1>
-                   <p style={{textAlign:"center"}}>Removing your personal information online</p>
-                    <p style={{textAlign:"center"}}>We continuously monitor and remove your personal information as it reappears.</p>
-                   <p style={{textAlign:"center"}}>Information that we want to be kept private is being shared in ways that can endanger our privacy, safety, well-being,<br/> and security. Many of us are shocked and upset when we find our  data being freely shared online without our <br/>knowledge or consent. </p>
+                   <h1 style={{paddingTop:"10%",textAlign:"center",color:"#FFFFFF",fontFamily:"Source Sans Pro",fontWeight:"bold"}}>WIPERTS</h1>
+                   <p style={{textAlign:"center"}} className="p1">Removing your personal information online</p>
+                    <p style={{textAlign:"center"}} className="p2">We continuously monitor and remove your personal information as it reappears.</p>
+                   <p style={{textAlign:"center"}} className="p3">Information that we want to be kept private is being shared in ways that can endanger our privacy, safety, well-being,<br/> and security. Many of us are shocked and upset when we find our  data being freely shared online without our <br/>knowledge or consent. </p>
                  </div>
                  <div  style={{paddingTop:"15%",paddingLeft:"30%"}}>
-              <a href="#details" ><div style={{height:"35px",borderRadius:"30px",borderColor:"10px black",marginLeft:"30%",backgroundColor:"black",width:"25px"}}>
+              <a href="#details" ><div style={{height:"35px",borderRadius:"30px",borderColor:"10px black",marginLeft:"30%",background:"linear-gradient(78.33deg, #034770 5.81%, #0F74B0 86.83%)",width:"25px"}}>
                    <p style={{padding:"30%",color:"white"}}>0</p></div>
                  <h5 style={{color:"white",fontFamily:"Source Sans Pro",marginLeft:"27%",fontWeight:"bolder"}}>More Info</h5>
                    </a>
@@ -34,16 +34,16 @@ export default function About(){
 
           <div className="thirdComponent">
               <div>
-                  <h1>200K+</h1>
-                  <p>Removal of personal information</p>
+                  <h1 className="h13">200K+</h1>
+                  <p className="h14">Removal of personal information</p>
               </div>
               <div>
-                  <h1>350K+</h1>
-                  <p>Removal of personal information</p>
+                  <h1 className="h13">350K+</h1>
+                  <p className="h14">Removal of personal information</p>
               </div>
               <div>
-                  <h1>99K+</h1>
-                  <p>Removal of personal information</p>
+                  <h1 className="h13">99K+</h1>
+                  <p className="h14">Removal of personal information</p>
               </div>
            </div>
 
